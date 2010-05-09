a296bd8a0597d4fbb538f5a73fd81a640c4e18d1
jobe <jobe@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-09 08:22:36 +0000

    added mmlib_xml
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mmlib/trunk@33060 735d13b6-9817-0410-8766-e36946ffe9aa

--- hooks/class.tx_mmlib_xml.php
+++ hooks/class.tx_mmlib_xml.php
@@ -13,7 +13,24 @@ class tx_mmlib_xml{
   /* PUBLICS: */
   
   function cObjGetSingleExt($name,$conf,$TSkey,$parent){
-    $content = sprintf('%s = %s(%s)<br/>',$TSkey,$name,get_class($this));
+    //$content = sprintf('%s = %s(%s)<br/>',$TSkey,$name,get_class($this));
+    
+    //$src = $parent->cObjGetSingle($conf['src'],$conf['src.'],'src');
+    if($conf['src.']) $conf['src'] = $parent->stdWrap($conf['src'],$conf['src.']);
+    
+    $xml = simplexml_load_string($conf['src']);
+    $data = array();
+    foreach($xml->children() as $key => $value) $data[$key][] = $value->asXML();
+    foreach($data as $key => $value) $data[$key] = implode(chr(0),$value);
+    
+    var_dump($data);
+    
+    $parent->data = $data;
+    
+    $content = $parent->cObjGetSingle($conf['renderObj'],$conf['renderObj.'],'renderObj');
+    
+    if ($conf['stdWrap.']) $content = $parent->stdWrap($content,$conf['stdWrap.']);// apply total stdWrap
+    
     return $content;
   }
   
