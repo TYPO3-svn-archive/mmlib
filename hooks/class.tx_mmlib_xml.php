e7ebfd9ed3cf1cd538d9724ab866665e03f0bec5
jobe <jobe@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-13 17:20:03 +0000

    added splitting
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mmlib/trunk@33168 735d13b6-9817-0410-8766-e36946ffe9aa

--- hooks/class.tx_mmlib_xml.php
+++ hooks/class.tx_mmlib_xml.php
@@ -2,39 +2,42 @@
 
 /**
  *  Handle xml data.
- *  Usage:
- *    10 = XML
- *    10.src = FILE
- *    10.src.file = fileadmin/...
  */
+
+define('SPLITCHAR',"\0");
 
 class tx_mmlib_xml{
 
-  /* PUBLICS: */
-  
   function cObjGetSingleExt($name,$conf,$TSkey,$parent){
-    //$content = sprintf('%s = %s(%s)<br/>',$TSkey,$name,get_class($this));
-    
-    //$src = $parent->cObjGetSingle($conf['src'],$conf['src.'],'src');
-    if($conf['src.']) $conf['src'] = $parent->stdWrap($conf['src'],$conf['src.']);
-    
-    $xml = simplexml_load_string($conf['src']);
-    $data = array();
-    foreach($xml->children() as $key => $value) $data[$key][] = $value->asXML();
-    foreach($data as $key => $value) $data[$key] = implode(chr(0),$value);
-    
-    var_dump($data);
-    
-    $parent->data = $data;
-    
-    $content = $parent->cObjGetSingle($conf['renderObj'],$conf['renderObj.'],'renderObj');
-    
-    if ($conf['stdWrap.']) $content = $parent->stdWrap($content,$conf['stdWrap.']);// apply total stdWrap
-    
+    if($conf['src.']){
+      $conf['src'] = $parent->cObjGetSingle($conf['src'],$conf['src.'],'src');
+      unset($conf['src.']);
+    }
+    print(t3lib_div::view_array($conf).'<hr>');
+    if(empty($conf['src']))return 'no src';
+    $content = '';
+    $parent->data = $this->getData($conf['src']);
+    $content .= $parent->cObjGetSingle($conf['renderObj'],$conf['renderObj.'],'renderObj');
+    if($conf['stdWrap.'])$content = $parent->stdWrap($content,$conf['stdWrap.']);// apply total stdWrap
     return $content;
   }
   
-  /* PRIVATES: */
+  function getData($src){
+    $xml = simplexml_load_string($src);
+    if(!$xml)return null;
+    $data = array();
+    foreach( $xml->children() as $key => $child){
+      $data['child:'.$key][] = $child->asXML();
+    }
+    foreach( $data as $key => $child ){
+      $data[$key] = implode(SPLITCHAR,$child);
+    }
+    foreach( $xml->attributes() as $key => $attribute){
+      $data[$key] = strval($attribute);
+    }
+    $data['this:name'] = $xml->getName();
+    return $data;
+  }
   
 }
 
