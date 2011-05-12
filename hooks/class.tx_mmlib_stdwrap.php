ea865dc39850c0e790c9f7595ad8f8390600b732
jobe <jobe@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-12 12:48:01 +0000

    added stdWrap "flexform"
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mmlib/trunk@47626 735d13b6-9817-0410-8766-e36946ffe9aa

--- hooks/class.tx_mmlib_stdwrap.php
+++ hooks/class.tx_mmlib_stdwrap.php
@@ -1,5 +1,4 @@
 <?php
-
 /***************************************************************
 *  Copyright notice
 *  [...]
@@ -9,33 +8,43 @@
 require_once(PATH_typo3.'/sysext/cms/tslib/interfaces/interface.tslib_content_stdwraphook.php');
 
 class tx_mmlib_stdwrap implements tslib_content_stdWrapHook {
-
+	
   private $listNumCount = null;
-
-	function stdWrapPreProcess($content, array $conf, tslib_cObj &$parentObject) {
+	
+  function stdWrapPreProcess($content, array $conf, tslib_cObj &$parentObject) {
     if($conf['listNum.']['stdWrap.']['rand']){
       $this->listNumCount = count(explode(',',$content));
     }
-		return $content;
-	}
-  
-	function stdWrapOverride($content, array $conf, tslib_cObj &$parentObject) {
-		return $content;
-	}
-  
-	function stdWrapProcess($content, array $conf, tslib_cObj &$parentObject) {
-		if($conf['rand']){
+    return $content;
+  }
+	
+  function stdWrapOverride($content, array $conf, tslib_cObj &$parentObject) {
+    return $content;
+  }
+	
+  function stdWrapProcess($content, array $conf, tslib_cObj &$parentObject) {
+		
+    if($conf['rand']){
       list($min,$max) = explode('|',$conf['rand']);
       if($max=='max') $max = ($this->listNumCount-1);
-			$content = rand($min,$max);
+      $content = rand($min,$max);
+    }
+		
+		if($conf['flexform']){
+			$pi_flexform = t3lib_div::xml2array($content);
+			$field 	= $conf['flexform'];
+			$sheet	= $conf['flexform.']['sheet']	? $conf['flexform.']['sheet']	: 'sDEF' ;
+			$lang		= $conf['flexform.']['lang']	? $conf['flexform.']['lang']	: 'lDEF' ;
+			$value	= $conf['flexform.']['value']	? $conf['flexform.']['value']	: 'vDEF' ;
+			$content = $pi_flexform['data'][$sheet][$lang][$field][$value];
 		}
-		return $content;
-	}
-  
-	function stdWrapPostProcess($content, array $conf, tslib_cObj &$parentObject) {
-		return $content;
-	}
-  
+		
+    return $content;
+  }
+	
+  function stdWrapPostProcess($content, array $conf, tslib_cObj &$parentObject) {
+    return $content;
+  }
 }
 
 ?>
