d6d7523a6a344fd2f1282f418169b514953a7db7
jobe <jobe@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-09 08:23:49 +0000

    added new cObj "QUERY"
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mmlib/trunk@66712 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,6 +8,7 @@ t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_mmlib_pi1.php','_pi1','list_typ
 $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_content.php']['cObjTypeAndClass'][] = array('XML',    'EXT:mmlib/hooks/class.tx_mmlib_xml.php:tx_mmlib_xml'       );
 $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_content.php']['cObjTypeAndClass'][] = array('REMOTE', 'EXT:mmlib/hooks/class.tx_mmlib_remote.php:tx_mmlib_remote' );
 $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_content.php']['cObjTypeAndClass'][] = array('DATA',   'EXT:mmlib/hooks/class.tx_mmlib_data.php:tx_mmlib_data'     );
+$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_content.php']['cObjTypeAndClass'][] = array('QUERY',  'EXT:mmlib/hooks/class.tx_mmlib_query.php:tx_mmlib_query'   );
 $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_content.php']['stdWrap'][] = 'EXT:mmlib/hooks/class.tx_mmlib_stdwrap.php:&tx_mmlib_stdwrap';
 $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][] =  'EXT:mmlib/hooks/class.tx_mmlib_pagerenderer.php:tx_mmlib_pagerenderer->renderPreProcess';
 //$TYPO3_CONF_VARS['SC_OPTIONS']['ext/direct_mail']['res/scripts/class.dmailer.php']['mailMarkersHook'] = array('EXT:mmlib/hooks/class.tx_mmlib_directmail.php:tx_mmlib_directmail->mailMarkersHook');
---
+++ hooks/class.tx_mmlib_query.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ *  Direct mapping to exec_SELECTgetRows()
+ *  
+ *  Usage:
+ *   10 = QUERY
+ *   10.fields    : 
+ *   10.table     : 
+ *   10.where     : 
+ *   10.groupBy   : 
+ *   10.orderBy   : 
+ *   10.limit     : 
+ *   10.renderObj : 
+ *   10.wrap      : 
+ *   10.stdWrap   : 
+ *   
+ */
+
+class tx_mmlib_query{
+	
+  function cObjGetSingleExt($name,$conf,$TSkey,$parent){
+    
+    if($conf['fields.'])  $conf['fields']   = $parent->stdWrap($conf['fields'],   $conf['fields.']  );
+    if($conf['table.'])   $conf['table']    = $parent->stdWrap($conf['table'],    $conf['table.']   );
+    if($conf['where.'])   $conf['where']    = $parent->stdWrap($conf['where'],    $conf['where.']   );
+    if($conf['groupBy.']) $conf['groupBy']  = $parent->stdWrap($conf['groupBy'],  $conf['groupBy.'] );
+    if($conf['orderBy.']) $conf['orderBy']  = $parent->stdWrap($conf['orderBy'],  $conf['orderBy.'] );
+    if($conf['limit.'])   $conf['limit']    = $parent->stdWrap($conf['limit'],    $conf['limit.']   );
+
+    $rows = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows(
+      $conf['fields']?$conf['fields']:'*',
+      $conf['table'],
+      $conf['where'],
+      $conf['groupBy'],
+      $conf['orderBy'],
+      $conf['limit']
+    );
+    
+    if($conf['renderObj']){// render each as specified
+      foreach($rows as $index => $row){
+        $parent->start($row,$conf['table']);
+        $content .= $parent->cObjGetSingle($conf['renderObj'],$conf['renderObj.']);
+      }
+    }else{// return amount of affected rows
+      $content .= count($rows);
+    }
+    
+    if($conf['wrap']) $content = $parent->wrap($content,$conf['wrap']);// enable total wrap
+    if($conf['stdWrap.']) $content = $parent->stdWrap($content,$conf['stdWrap.']);// enable total stdWrap
+    
+    return $content;
+  }
+	
+}
+
+?>
